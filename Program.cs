afea97b8740189afe9a9a9488b45d5ab00065adc
Trung Nguyen <trung150502@gmail.com>
2023-11-30 21:47:19 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-using System;
+﻿using System;
 
-namespace IDM
+namespace ConsoleApplication
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Start Chuong trinh!!");
+        public static void Main(string[] args)
+        {   
+            Console.Clear();
+            Console.Title = "File Manager Console Application";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("   ___ _ _                                                     ___  __    ___   ____    ___  ");
+            Console.WriteLine("  / __(_) | ___    /\\/\\   __ _ _ __   __ _  __ _  ___ _ __    / _ \\/__\\  /___\\ |___ \\  / _ \\ ");
+            Console.WriteLine(" / _\\ | | |/ _ \\  /    \\ / _` | '_ \\ / _` |/ _` |/ _ \\ '__|  / /_)/ \\// //  //   __) || | | |");
+            Console.WriteLine("/ /   | | |  __/ / /\\/\\ \\ (_| | | | | (_| | (_| |  __/ |    / ___/ _  \\/ \\_//   / __/ | |_| |");
+            Console.WriteLine("\\/    |_|_|\\___| \\/    \\/\\__,_|_| |_|\\__,_|\\__, |\\___|_|    \\/   \\/ \\_/\\___/   |_____(_)___/ ");
+            Console.WriteLine("                                           |___/                                             ");
+            Console.WriteLine("Welcome to File Manager PRO! To proceed, select an option from the menu below using the UP and DOWN arrow keys to navigate:");
+            Console.WriteLine();      
         }
     }
 }

1cac5230b0e2f1a8d24c771d798d2a2fa5fc38d3
Trung Nguyen <trung150502@gmail.com>
2023-12-01 21:46:01 +0700

    Add files via upload

---
+++ ProgramHttpClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Http.Headers;
+
+const string uri = "https://khodattenqua.azurewebsites.net/api/HttpTrigger1";
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json")
+);
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    var json = await client.PostAsync(uri,
+        JsonContent.Create(
+            new Request{name = "HIHIHIHI"},
+            new MediaTypeWithQualityHeaderValue("application/json")
+        )
+);
+    Console.Write(json.Content);
+}
+
+
+class Request {
+    public string name { get; set; }
+}

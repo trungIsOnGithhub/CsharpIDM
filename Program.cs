ae511ea67db9fcac88711a589af7f9eeea6a988d
trungvuive <trung150502@gmail.com>
2023-12-02 11:43:57 +0700

    modified main display

--- Program.cs
+++ Program.cs
@@ -7,16 +7,17 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {   
             Console.Clear();
-            Console.Title = "File Manager Console Application";
+            Console.Title = "Console Ultilities Application";
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("   ___ _ _                                                     ___  __    ___   ____    ___  ");
-            Console.WriteLine("  / __(_) | ___    /\\/\\   __ _ _ __   __ _  __ _  ___ _ __    / _ \\/__\\  /___\\ |___ \\  / _ \\ ");
-            Console.WriteLine(" / _\\ | | |/ _ \\  /    \\ / _` | '_ \\ / _` |/ _` |/ _ \\ '__|  / /_)/ \\// //  //   __) || | | |");
-            Console.WriteLine("/ /   | | |  __/ / /\\/\\ \\ (_| | | | | (_| | (_| |  __/ |    / ___/ _  \\/ \\_//   / __/ | |_| |");
-            Console.WriteLine("\\/    |_|_|\\___| \\/    \\/\\__,_|_| |_|\\__,_|\\__, |\\___|_|    \\/   \\/ \\_/\\___/   |_____(_)___/ ");
-            Console.WriteLine("                                           |___/                                             ");
-            Console.WriteLine("Welcome to File Manager PRO! To proceed, select an option from the menu below using the UP and DOWN arrow keys to navigate:");
-            Console.WriteLine();      
+            Console.WriteLine("┬─┬ノ( º _ ºノ)");
+            Console.WriteLine( Utilities.GetSystemInfo() );
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Welcome to Console Ultilities Application! Select an option from below menu using the UP and DOWN arrow keys to navigate:");
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+
+            DynamicMenu.Menu(new []{"option1", "option2", "option3"}, 1);
         }
     }
 }

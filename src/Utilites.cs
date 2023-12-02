f54b463497bf0dc839e8c928ce4b21f5819a3e0e
trungvuive <trung150502@gmail.com>
2023-12-02 11:44:27 +0700

    added system info text and check valid path method

--- src/Utilites.cs
+++ src/Utilites.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApplication
 {
@@ -24,6 +27,38 @@ namespace ConsoleApplication
                 double rounded = Math.Round(kilobytes, 2);
                 return (rounded + "KB");
             }
+        }
+
+        public static bool IsValidPath(string path)
+        {
+            string[] fileOrDirNames = path.Split(Path.DirectorySeparatorChar);
+
+            Regex fileNamePattern = new Regex(@"^[\w\s_.:]*$");
+            
+            foreach (string fileOrDirName in fileOrDirNames)
+            {
+                Match matchResult = fileNamePattern.Match(fileOrDirName);
+                
+                if (matchResult == Match.Empty)
+                {
+                return false;
+                }
+            }
+            
+            return true;
+        }
+
+        public static string GetSystemInfo()
+        {
+            StringBuilder systemInfo = new StringBuilder(string.Empty);
+    
+            systemInfo.AppendFormat("OS Type:\t\t{0}\n", Environment.OSVersion);
+            systemInfo.AppendFormat("Version:\t\t{0}\n", Environment.Version);
+            systemInfo.AppendFormat("Architecture:\t\t{0}\n", Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE"));
+            systemInfo.AppendFormat("SystemDirectory:\t\t{0}\n", Environment.SystemDirectory);
+            systemInfo.AppendFormat("UserName:\t\t{0}\n", Environment.UserName);
+
+            return systemInfo.ToString();
         }
     }
 }

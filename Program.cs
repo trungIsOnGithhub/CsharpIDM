b10ff1ad3c63c61090f33ffb5108b938f961b4c8
trungvuive <trung150502@gmail.com>
2023-12-04 09:52:53 +0700

    update program for testing function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ConsoleApplication
 {
@@ -9,15 +10,45 @@ namespace ConsoleApplication
             Console.Clear();
             Console.Title = "Console Ultilities Application";
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("┬─┬ノ( º _ ºノ)");
+            // Console.WriteLine("┬─┬ノ( º _ ºノ)");
             Console.WriteLine( Utilities.GetSystemInfo() );
-            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------");
+            // Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------");
             Console.WriteLine("Welcome to Console Ultilities Application! Select an option from below menu using the UP and DOWN arrow keys to navigate:");
-            Console.WriteLine();
-            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------");
+            // Console.WriteLine();
+            // Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------");
             Console.WriteLine();
 
-            DynamicMenu.Menu(new []{"option1", "option2", "option3"}, 1);
+            // DynamicMenu.Menu(DynamicMenu.mainMenu, 1);
+
+            Console.Clear();
+
+            StringBuilder charBuffer = new StringBuilder(String.Empty);
+
+            while (true) {
+                ConsoleKeyInfo keyPress = Console.ReadKey(true);
+                
+                char keyRead = Convert.ToChar(keyPress.Key);
+
+                char keyReadFiltered = Utilities.filterKey(keyRead);
+                
+                charBuffer.Append(keyRead);
+
+                Console.Write(keyRead);
+
+                if (keyPress.Key == ConsoleKey.Enter)
+                {
+                    // string line = Console.ReadLine();
+                    Console.WriteLine();
+                    CocktailModel model = new CocktailModel { VersionString = "v1" };
+                    model.getCocktailByFirstLetter(charBuffer[0]);
+                    Console.WriteLine();
+
+                    // MessageModel.save(charBuffer.ToString());
+                    Console.WriteLine();
+                    Console.WriteLine($"Enter Pressed: {charBuffer.ToString()}");
+                    // break;
+                }
+            }
         }
     }
 }

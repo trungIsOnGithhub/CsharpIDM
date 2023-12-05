2e7873f196d155e826ab84bf52d1e4d10badb7ce
trung <trung150502@gmail.com>
2023-12-05 19:23:25 +0700

    add generics

---
+++ src/Generics/Enumerable.cs
@@ -0,0 +1,40 @@
+using System.Text;
+
+namespace Generics
+{
+    class EnumerablePrint<T>   
+    {
+        public static void printEnumerableByLine(IEnumerable<T> enummerable)
+        {
+            Console.WriteLine("[");
+            foreach (T item in enummerable)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("]");
+        }
+
+        public static void printEnumerableByComma(IEnumerable<T> enumerable)
+        {
+            StringBuilder toPrint = new StringBuilder("[");
+
+            bool moreThanOneElement = false;
+
+            foreach (T item in enumerable)
+            {
+                toPrint.Append($"{item}, ");
+                moreThanOneElement = true;
+            }
+
+            if (moreThanOneElement)
+            {
+                toPrint.Remove(toPrint.Length - 2, 2);
+                // Console.WriteLine(toPrint);
+            }
+
+            toPrint.Append(']');
+
+            Console.WriteLine(toPrint);
+        }
+    }
+}
